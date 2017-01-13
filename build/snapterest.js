ac76493f8539bb0209acc77017adf4e4e8fc198d
Estefano Castañeda <mixc21@gmail.com>
2017-01-13 17:07:06 -0500

    update tutorial

--- build/snapterest.js
+++ build/snapterest.js
@@ -20484,8 +20484,28 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactClass = React.createClass({
+  getInitialState: function () {
+    return { isHeaderHidden: false,
+      title: 'Stateful React Component'
+    };
+  },
+  handleClick: function () {
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden });
+  },
   render: function () {
-    return React.createElement('h1', { className: 'header' }, 'React Component');
+    var headerElement = React.createElement('h1', {
+      className: 'header', key: 'header'
+    }, this.state.title);
+    var buttonElement = React.createElement('button', {
+      className: 'btn btn-default',
+      onClick: this.handleClick,
+      key: 'button' }, 'Toggle header');
+
+    if (this.state.isHeaderHidden) {
+      return React.createElement('div', null, [buttonElement]);
+    }
+    return React.createElement('div', null, [buttonElement, headerElement]);
   }
 });
 
--- source/app.js
+++ source/app.js
@@ -2,10 +2,30 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactClass = React.createClass({
-  render: function () {
-    return  React.createElement('h1', {className: 'header'}, 'React Component');
-  }
-});
+  getInitialState: function () {
+    return { isHeaderHidden: false,
+      title: 'Stateful React Component'
+    };
+  },
+  handleClick: function () {
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden });
+    },
+    render: function () {
+      var headerElement = React.createElement('h1', {
+        className: 'header', key: 'header'
+      }, this.state.title);
+      var buttonElement = React.createElement('button', {
+        className: 'btn btn-default',
+        onClick: this.handleClick,
+        key: 'button' }, 'Toggle header');
+
+        if (this.state.isHeaderHidden) {
+          return React.createElement('div', null, [buttonElement]);
+        }
+        return React.createElement('div', null, [ buttonElement, headerElement ]);
+      }
+  });
 
 var reactComponentElement = React.createElement (ReactClass);
 var selector = document.getElementById('react-application');
